bd5d4d149ebf27f52c625b06777dc66124b5289a
dfbertdev <dfbertdev@gmail.com>
2025-03-13 18:32:59 -0300

    Alterando deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  
+  nitro: {
+    preset: 'static', // Garante que o Nuxt gere um build estático
+    prerender: {
+      routes: ['/', '/200.html', '/404.html'], // Adiciona a rota principal para garantir que seja gerada
+    }
+  }
 })
+
